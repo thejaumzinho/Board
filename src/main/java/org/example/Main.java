e7123f066725c23fee91c7cb8bb468ff89442139
Joao <joaogabedev@gmail.com>
2025-02-21 17:04:12 -0300

    Final Setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,17 @@
 package org.example;
-
-import org.example.persistance.config.ConnectionConfig;
-import org.example.persistance.migration.MigrationStrategy;
-
+import org.example.persistence.migration.MigrationStrategy;
+import org.example.ui.MainMenu;
 import java.sql.SQLException;
+import static org.example.persistence.config.ConnectionConfig.getConnection;
 
 public class Main {
-    public static void main(String[] args) throws SQLException{
-
-        try(var connection = ConnectionConfig.getConnection())
+    public static void main(String[] args) throws SQLException
+    {
+        try(var connection = getConnection())
         {
             new MigrationStrategy(connection).executeMigration();
         }
+        new MainMenu().execute();
     }
+
 }
